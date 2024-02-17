5348242f7f2a7edd62a9370f6c30b7b71cf46747
looee <transistor42b@yandex.ru>
2024-02-17 20:30:31 +0300

    task_11: добавил установку темы приложения в зависимости от выбранной темы в телефоне

--- app/src/main/java/ru/practicum/android/diploma/ui/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/App.kt
@@ -1,5 +1,17 @@
 package ru.practicum.android.diploma.ui
 
 import android.app.Application
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatDelegate
 
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(getPhoneNightModeValue())
+    }
+
+    private fun getPhoneNightModeValue(): Int {
+        return applicationContext.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)
+    }
+}
